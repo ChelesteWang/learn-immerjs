ef7b913e45c5072a38b7e8da6330df80bb150f05
ChelesteWang <1581446178@qq.com>
2022-01-31 16:50:52 +0800

    1 file modified

--- src/feature-1/index.ts
+++ src/feature-1/index.ts
@@ -1,13 +1,24 @@
+/**
+ * 初始化数据源
+ */
 const obj = {
   name: 'xiaoming',
   age: 12,
   gender: 'man',
 };
 
+type IGender = 'man' | 'woman';
+
+/**
+ * 创建一个领域对象
+ * @param name
+ * @param age
+ * @param gender
+ */
 class Person {
   private name: string;
   private age: number;
-  private gender: 'man' | 'woman';
+  private gender: IGender;
 
   constructor({ name, age, gender }) {
     this.name = name;
@@ -18,6 +29,24 @@ class Person {
   getPerson() {
     return { name: this.name, age: this.age, gender: this.gender };
   }
+
+  setName(name: string) {
+    this.name = name;
+  }
+
+  setAge(age: number) {
+    this.age = age;
+  }
+
+  setGender(gender: IGender) {
+    this.gender = gender;
+  }
 }
 
+/**
+ * 新建一个类型
+ * @param Person
+ */
 const xiaoming = new Person(obj);
+
+console.log(xiaoming.getPerson());
